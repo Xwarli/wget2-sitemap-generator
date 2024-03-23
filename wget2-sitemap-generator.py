6d7552f7134819e6867c73aa5f10a5a72a3d3163
Xwarli <104170778+Xwarli@users.noreply.github.com>
2024-03-23 11:17:23 +0000

    Update wget2-sitemap-generator.py

--- wget2-sitemap-generator.py
+++ wget2-sitemap-generator.py
@@ -1,13 +1,24 @@
+# --------------------------------------------------------------------------------------
+# 	Witten by Marley Sexton | Github: Xwarli
+# 	CC0 License (Public Domain)
+# --------------------------------------------------------------------------------------
 import subprocess
 import re
 import os
 
 def generate_sitemap(url):
+	# creates the log file and sitemap (sm) file names
+	#	example: http://example.com --> http:__example-com[-log/sitemap].txt
 	logfile = url.replace("/", "_").replace(".","-") + "-log.txt"
-	smfile = url.replace("/", "_").replace(".","-") + "sitemap.txt"
+	smfile = url.replace("/", "_").replace(".","-") + "-sitemap.txt"
+
+	# creates the command. wget2, no parent, robots off, wait 1sc, mirror, spider
+	# outputs all to the logfile above
 	command = "wget2 " + url + " -np -e robots=off -w 1 -m --spider -o " + logfile
+	
+	# prints the command for user feedback/error spotting, then runs the subprocess
 	print(command)
-	subprocess.run(command, shell=Truew)
+	subprocess.run(command, shell=True)
 
 	# Parse log file to extract URLs
 	urls = set()
@@ -19,15 +30,16 @@ def generate_sitemap(url):
 				url = match.group(1).strip()
 				urls.add(url)
 
-	# Write URLs to sitemap file
+	# Write URLs to the prenamed sitemap file
 	with open(smfile, 'w') as sitemap_file:
 		for url in urls:
 			sitemap_file.write(url + '\n')
 
+	# user feeback
 	print("Sitemap generated successfully.")
 
 	# Clean up downloaded files
 	os.system('rm -rf ' + os.path.join(os.getcwd(), url.split('//')[1]))
 
-# Example usage:
-generate_sitemap(input(">>>>"))
+# Gets user input for the starting URL
+generate_sitemap(input("Starting URL: "))
